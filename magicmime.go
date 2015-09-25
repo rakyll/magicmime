1fc0b205c1e6f072c5b64c56231f293caec0d5b8
Burcu Dogan <jbd@google.com>
2015-09-25 13:43:27 -0700

    Only linux and darwin are supported.

--- magicmime.go
+++ magicmime.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +build linux darwin
+
 // Package magicmime detects mimetypes using libmagic.
 package magicmime
 
